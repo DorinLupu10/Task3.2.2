6f95c482bd8833fb4cb743f0b00e64c38a47e55a
Dorin Lupu <dorin.lupu@outlook.com>
2026-05-03 19:39:32 +0300

    edit

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "aws_region" {
   default = "eu-north-1"
-  type        = string
+  type    = string
 }
 
 variable "bucket_name" {
   default = "dorin-terraform-test-bucket-449024774937"
-  type        = string
+  type    = string
 }
